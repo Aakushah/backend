1493dc1cb85ff06002976dcc8c7f9f655994f193
Äakash prasad sah <aakushah2@gmail.com>
2023-12-29 08:34:26 +0530

    fix the secret leak

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ connectDB()
 /*
 (async ()=>{
     try{
-       const connectionInstance=await mongoose.connect("mongodb+srv://aaku:aaku@cluster0.fd3qayt.mongodb.net/")
+       const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
        console.log(`\n MONGODB CONNECTED !! DB HOST: ${connectionInstance.connection.host} `)
     }
     catch(error){
